88d543fae4d280e13a56f4a2784865a007a78ca3
Kaizeras <=>
2017-09-25 13:53:31 +0300

    fixing a typo

--- Angular4/play-ground/app/app-routing.module.ts
+++ Angular4/play-ground/app/app-routing.module.ts
@@ -4,21 +4,21 @@ import { FailComponent } from "./fail/fail.component"
 import { SelectivePreloadingStrategy } from "./selective-preloading-strategy";
 
 let routes: Routes = [
-  { path: 'contacts',   loadChildren: 'app/contacts/contacts.module#ContactsModule' },
-  { path: 'about',      loadChildren: 'app/about/about.module#AboutModule', data: { preload: true } },
-  { path: '',           redirectTo: '/contacts', pathMatch: 'full' },
-  { path: '**',         component: FailComponent }
-]
+    { path: 'contacts',   loadChildren: 'app/contacts/contacts.module#ContactsModule' },
+    { path: 'about',      loadChildren: 'app/about/about.module#AboutModule', data: { preload: true } },
+    { path: '',           redirectTo: '/contacts', pathMatch: 'full' },
+    { path: '**',         component: FailComponent }
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(
-                routes, 
-                { 
-                  useHash: true,
-                  preloadingStrategy: SelectivePreloadingStrategy  
-                }) 
-           ],
-  exports: [ RouterModule ],
-  providers: [ SelectivePreloadingStrategy ]
+    imports: [ RouterModule.forRoot(
+        routes,
+        {
+            useHash: true,
+            preloadingStrategy: SelectivePreloadingStrategy
+        })
+    ],
+    exports: [ RouterModule ],
+    providers: [ SelectivePreloadingStrategy ]
 })
 export class AppRoutingModule {}
