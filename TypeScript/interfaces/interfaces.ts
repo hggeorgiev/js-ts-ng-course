689b6ebb68558de5f441e5a730ad0d6fbeb05055
hggeorgiev <hgeorgievkzr@gmail.com>
2018-10-14 00:49:01 +0200

    feat: Update tutorial to TypeScript 3.1

--- TypeScript/interfaces/interfaces.ts
+++ TypeScript/interfaces/interfaces.ts
@@ -1,208 +1,298 @@
 // -- Simple --
-
-interface LabelledValue {
+{
+  interface LabelledValue {
     label: string;
-    size?:number;
-}
+    size?: number;
+  }
 
-function printLabel(labelledObj: LabelledValue) {
+
+// { label: string }
+
+  function printLabel(labelledObj: LabelledValue) {
     console.log(labelledObj.label);
-}
+  }
 
-let myObj = {size: 10, label: "Size 10 Object"};
-printLabel(myObj);
+  let myObj = {size: 10, label: "Size 10 Object"};
+  printLabel(myObj);
+}
+// Only the shape that matters - if the requirements are met, it wll pass.
+// The type-checker does not require that these properties come in any sort of order,
+// only that the properties the interface requires are present and have the required type.
 
 
 // -- Optional Properties --
-
-interface SquareConfig {
+{
+  interface SquareConfig {
     color?: string;
     width?: number;
-}
+  }
 
-function createSquare(config: SquareConfig): {color: string; area: number} {
+  function createSquare(config: SquareConfig): { color: string; area: number } {
     let newSquare = {color: "white", area: 100};
     if (config.color) {
-        newSquare.color = config.color;
+      newSquare.color = config.color;
     }
     if (config.width) {
-        newSquare.area = config.width * config.width;
+      newSquare.area = config.width * config.width;
     }
     return newSquare;
-}
+  }
 
-let mySquare = createSquare({color: "black"});
+  let mySquare = createSquare({color: "black"});
+
+}
+// -- Readonly --
+{
+  interface Point {
+    readonly x: number;
+    readonly y: number;
+  }
+
+  let p1: Point = {x: 10, y: 20};
+  p1.x          = 5; // error!
+}
+// Readonly array
+// Readonly is the 'const' for properties
+{
+  let a: number[]               = [1, 2, 3, 4];
+  let ro: ReadonlyArray<number> = a;
+  ro[0]                         = 12; // error!
+  ro.push(5); // error!
+  ro.length = 100; // error!
+  a         = ro; // error!
+}
 
 // -- Excess Property Checks --
+{
+  let mySquare2 = createSquare({colour: "red", width: 100});
 
-let mySquare2 = createSquare({ colour: "red", width: 100 });
+  interface SquareConfig {
+    color?: string;
+    width?: number;
 
-// -- Function Type --
-
-interface SearchFunc {
-    (source: string, subString: string): boolean;
+    [propName: string]: any; //an alternative way to define extra 'unknown' values
+  }
 }
+// -- Function Type --
+{
+  interface SearchFunc {
+    (source: string, subString: string): boolean;
+  }
 
-let mySearch: SearchFunc;
-mySearch = function(src: string, sub: string ) { // parameter names may not match
+  let mySearch: SearchFunc;
+  mySearch = function (src: string, sub: string) { // parameter names may not match
     let result = src.search(sub);
     if (result == -1) {
-        return false;
+      return false;
     }
     else {
-        return true;
+      return true;
     }
+  }
 }
-
 // -- Indexable Types --
+{
+  interface StringArray {
+    [index: number]: string; //index can be only string or number
+  }
 
-interface StringArray {
-    [index: number]: string;
+  let myArray: StringArray;
+  myArray = ["Bob", "Fred"];
+
+  let myStr: string = myArray[0];
+
+  class Animal {
+    name: string;
+  }
+
+  class Shark extends Animal {
+    breed: string;
+  }
+
+
+// Error: indexing with a numeric string might get you a completely separate type of Animal!
+  interface NotOkay {
+    [x: number]: Animal;
+
+    [x: string]: Shark;
+  }
+}
+// The right way
+{
+  interface NumberDictionary {
+    [index: string]: number;
+
+    length: number;    // ok, length is a number
+    name: string;      // error, the type of 'name' is not a subtype of the indexer
+  }
 }
 
-let myArray: StringArray;
-myArray = ["Bob", "Fred"];
+// Readonly indexing
+{
+  interface ReadonlyStringArray {
+    readonly [index: number]: string;
+  }
 
-let myStr: string = myArray[0];
-
+  let myArrayIndx: ReadonlyStringArray = ["Alice", "Bob"];
+  myArrayIndx[2]                       = "Mallory"; // error!
+}
 // -- Class Types --
-
 {
+  {
     interface ClockInterface {
-        currentTime: Date;
+      currentTime: Date;
     }
 
     class Clock implements ClockInterface {
-        currentTime: Date;
-        constructor(h: number, m: number) { }
+      currentTime: Date;
+
+      constructor(h: number, m: number) {
+      }
     }
+  }
 }
-
-// method is the interface
+// Method is the interface
 {
-    interface ClockInterface {
-        currentTime: Date;
-        setTime(d: Date);
+  interface ClockInterface {
+    currentTime: Date;
+
+    setTime(d: Date);
+  }
+
+  class Clock implements ClockInterface {
+    currentTime: Date;
+
+    setTime(d: Date) {
+      this.currentTime = d;
     }
 
-    class Clock implements ClockInterface {
-        currentTime: Date;
-        setTime(d: Date) {
-            this.currentTime = d;
-        }
-        constructor(h: number, m: number) { }
+    constructor(h: number, m: number) {
     }
+  }
 }
 
 
 // -- Difference between the static and instance sides of classes --
 {
-    interface ClockConstructor {
-        new (hour: number, minute: number);
-    }
+  interface ClockConstructor {
+    new (hour: number, minute: number);
+  }
 
-    class Clock implements ClockConstructor {
-        currentTime: Date;
-        constructor(h: number, m: number) { }
+  class Clock implements ClockConstructor {
+    currentTime: Date;
+
+    constructor(h: number, m: number) {
     }
+  }
 }
 
 // solution
 
 {
-    interface ClockConstructor {
-        new (hour: number, minute: number): ClockInterface;
-    }
-    interface ClockInterface {
-        tick();
+  interface ClockConstructor {
+    new (hour: number, minute: number): ClockInterface;
+  }
+
+  interface ClockInterface {
+    tick();
+  }
+
+  function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, minute);
+  }
+
+  class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) {
     }
 
-    function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
-        return new ctor(hour, minute);
+    tick() {
+      console.log("beep beep");
+    }
+  }
+
+  class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) {
     }
 
-    class DigitalClock implements ClockInterface {
-        constructor(h: number, m: number) { }
-        tick() {
-            console.log("beep beep");
-        }
+    tick() {
+      console.log("tick tock");
     }
-    class AnalogClock implements ClockInterface {
-        constructor(h: number, m: number) { }
-        tick() {
-            console.log("tick tock");
-        }
-    }
+  }
 
-    let digital = createClock(DigitalClock, 12, 17);
-    let analog = createClock(AnalogClock, 7, 32);
+  let digital = createClock(DigitalClock, 12, 17);
+  let analog  = createClock(AnalogClock, 7, 32);
 }
 
 // -- Extending Interfaces --
-
-interface Shape {
-    color: string;
-}
-
-interface PenStroke {
-    penWidth: number;
-}
-
-interface Square extends Shape, PenStroke {
-    sideLength: number;
-}
-
-let square = <Square>{};
-square.color = "blue";
-square.sideLength = 10;
-square.penWidth = 5.0;
-
-// -- Hybrid Types --
-
-interface Counter {
-    (start: number): string;
-    interval: number;
-    reset(): void;
-}
-
-function getCounter(): Counter {
-    let counter = <Counter>function (start: number) { };
-    counter.interval = 123;
-    counter.reset = function () { };
-    return counter;
-}
-
-let c = getCounter();
-c(10);
-c.reset();
-c.interval = 5.0;
-
-// -- Interfaces Extending Classes --
-
 {
-    class Control {
-        private state: any;
-    }
+  interface Shape {
+    color: string;
+  }
 
-    interface SelectableControl extends Control {
-        select(): void;
-    }
+  interface PenStroke {
+    penWidth: number;
+  }
 
-    class Button extends Control {
-        select() {
-        }
-    }
+  interface Square extends Shape, PenStroke {
+    sideLength: number;
+  }
 
-    class TextBox extends Control {
-        select() {
-        }
-    }
+  let square        = <Square>{};
+  square.color      = "blue";
+  square.sideLength = 10;
+  square.penWidth   = 5.0;
+}
+// -- Hybrid Types --
+{
+  interface Counter {
+    (start: number): string;
 
-    class Image extends Control {
-    }
+    interval: number;
 
-    class Location {
-        select() {
-        }
+    reset(): void;
+  }
+
+  function getCounter(): Counter {
+    let counter      = <Counter>function (start: number) {
+    };
+    counter.interval = 123;
+    counter.reset    = function () {
+    };
+    return counter;
+  }
+
+  let c = getCounter();
+  c(10);
+  c.reset();
+  c.interval = 5.0;
+}
+// -- Interfaces Extending Classes --
+// Interfaces it inherits the members of the class, not implementation.
+// Interfaces inherit even the private and protected members of a base class, making it bound to the class and its subclasses.
+{
+
+  class Control {
+    private state: any;
+  }
+
+  interface SelectableControl extends Control {
+    select(): void;
+  }
+
+  class Button extends Control implements SelectableControl {
+    select() {
     }
+  }
+
+  class TextBox extends Control {
+    select() {
+    }
+  }
+
+// Error: Property 'state' is missing in type 'Image'.
+  class Image implements SelectableControl {
+    select() {
+    }
+  }
+
 }
--- TypeScript/variable-declarations/variable-declarations.ts
+++ TypeScript/variable-declarations/variable-declarations.ts
@@ -1,4 +1,35 @@
-// -- Var scoping rules and quirks --
+// Variable capturing
+function f() {
+  var a = 10;
+  return function g() {
+    var b = a + 1;
+    return b;
+  }
+}
+
+var g = f();
+g(); // returns '11'
+
+// In this above example, g captured the variable a declared in f.
+// At any point that g gets called, the value of a will be tied to the value of a in f.
+// Even if g is called once f is done running, it will be able to access and modify a
+function f() {
+  var a = 1;
+
+  a = 2;
+  var b = g();
+  a = 3;
+
+  return b;
+
+  function g() {
+    return a;
+  }
+}
+
+f(); // returns '2'
+
+// Variable scoping rules and quirks
 
 function f(shouldInitialize: boolean) {
     if (shouldInitialize) {
@@ -10,8 +41,9 @@ function f(shouldInitialize: boolean) {
 
 document.write( '' + f(true) + '<br/>' );  // returns '10'
 document.write( '' + f(false) ); // returns 'undefined'
+// 'var' can be accessed outside of a block.
 
-// in nested loops
+// In nested loops
 
 function printMatrix() {
     for (var i = 0; i < 5; i++) {
@@ -23,39 +55,112 @@ function printMatrix() {
 }
 printMatrix()
 
-// -- Variable capturing quirks --
+
+// Variable capturing quirks
 
 for (var i = 0; i < 10; i++) {
     setTimeout(function() {console.log(i); }, 100 * i);
 }
 
 
-// -- Block/Lexical scoping ---
-try {
-    throw "oh no!";
-}
-catch (e) {
-    console.log("Oh well.");
-}
+// Block/Lexical scoping
+// Unlike variables declared with var whose scopes leak out to their containing function,
+// block-scoped variables are not visible outside of their nearest containing block or for-loop.
 
-// Error: 'e' doesn't exist here
-console.log(e);
+function f(input: boolean) {
+  let a = 100;
 
-// --
+  if (input) {
+    // Still okay to reference 'a'
+    let b = a + 1;
+    return b;
+  }
+
+  // Error: 'b' doesn't exist here
+  return b;
+}
+// 'let' cannot be accessed outside of the block
 
 function foo() {
     // okay to capture 'a'
     return a;
 }
 
-// illegal call 'foo' before 'a' is declared
-// runtimes should throw an error here
+// Illegal call 'foo' before 'a' is declared
 foo();
 
 let a;
 
 
-// -- Const --
+// Re-declarations and shadowing
+function f(x) {
+  var x;
+  var x;
 
+  if (true) {
+    var x;
+  }
+}
+// You can re-declare a 'var' as many times as you wish.
+
+let x = 10;
+let x = 20; // error: can't re-declare 'x' in the same scope
+// You cannot re-declare a 'let' in the same scope
+
+// Shadowing
+//The act of introducing a new name in a more nested scope is called shadowing.
+// It is a bit of a double-edged sword in that it can introduce certain bugs on its own in the event of accidental shadowing,
+// while also preventing certain bugs. For instance, imagine we had written our earlier sumMatrix function using let variables.
+
+function sumMatrix(matrix: number[][]) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    var currentRow = matrix[i];
+    for (let i = 0; i < currentRow.length; i++) {
+      sum += currentRow[i];
+    }
+  }
+
+  return sum;
+}
+// Block-scoped 'let' works as expected.
+
+// Variable capturing (capturing 'let')
+function theCityThatAlwaysSleeps() {
+  let getCity;
+
+  if (true) {
+    let city = "Seattle";
+    getCity = function() {
+      return city;
+    }
+  }
+
+  return getCity();
+}
+
+// Works without IIFE because let is executed only in the block
+// The TypeScript compiler automatically adds an IIFE when transpiling to JS
+for (let i = 0; i < 10 ; i++) {
+  setTimeout(function() { console.log(i); }, 100 * i);
+}
+
+
+// Const
 const numLivesForCat = 9;
-numLivesForCat = 10; // displays error but value is still changed
+const kitty = {
+  name: "Aurora",
+  numLives: numLivesForCat,
+}
+
+// Error
+kitty = {
+  name: "Danielle",
+  numLives: numLivesForCat
+};
+
+// all "okay"
+kitty.name = "Rory";
+kitty.name = "Kitty";
+kitty.name = "Cat";
+kitty.numLives--;
