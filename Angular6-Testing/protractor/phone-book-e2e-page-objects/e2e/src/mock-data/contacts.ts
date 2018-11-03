38bace950fdc8a2b99b1a36d63dbea8fecee91f6
hggeorgiev <hgeorgievkzr@gmail.com>
2018-11-03 12:42:49 +0100

    feat: break down scenario exercises for protractor

---
+++ Angular6-Testing/protractor/phone-book-e2e-page-objects/e2e/src/mock-data/contacts.ts
@@ -0,0 +1,12 @@
+export const contactEdit = {
+  firstName: 'Java',
+  lastName: 'Jarawal',
+  email: 'java@paysafe.com'
+};
+
+
+export const contactAdd = {
+  firstName: 'Add',
+  lastName: 'Added',
+  email: 'addcontact@paysafe.com'
+};
---
+++ Angular6-Testing/protractor/phone-book-e2e-scenario/e2e/src/edit-contact.e2e-spec.ts
@@ -0,0 +1,68 @@
+import { browser, by, element, protractor } from "protractor";
+
+describe('Editing a contact', () => {
+  const list = element(by.tagName('contacts-list')).all(by.tagName('li'));
+
+  beforeAll(() => {
+    browser.get('#');
+  });
+
+  it('Should select Alyx VANCE', () => {
+    list.get(1).element(by.tagName('a')).click();
+    const details = element(by.id('contactsDetailsContainer'));
+    let txt       = details.getText();
+    // Needs https://github.com/JamieMason/Jasmine-Matchers installed to work properly
+    expect(txt).toEqual(jasmine.stringMatching('Alyx'));
+    expect(txt).toEqual(jasmine.stringMatching('Vance'));
+    expect(txt).toEqual(jasmine.stringMatching('alyx@resitance.com'));
+  });
+
+  it('Should display a form contact', () => {
+    list.get(1).element(by.tagName('a')).click();
+    const editBtn = element(by.id('edit-btn'));
+    editBtn.click();
+
+    const form = element(by.tagName('form'));
+
+    expect(form.isPresent()).toBeTruthy();
+  });
+  
+  it('Should edit contact', () => {
+    const form = element(by.tagName('form'));
+    expect(form.getAttribute('class')).toContain('ng-valid');
+  });
+
+  it('Should edit first name', () => {
+    const firstNameField = element(by.name('firstName'));
+    firstNameField.clear();
+    firstNameField.sendKeys('Java');
+  });
+
+  it('Should edit last name', () => {
+    const lastNameField  = element(by.name('lastName'));
+    lastNameField.clear();
+    lastNameField.sendKeys('Jarawal');
+  });
+
+  it('Should edit email', () => {
+    const emailField     = element(by.name('email'));
+    emailField.clear();
+    emailField.sendKeys('java@paysafe.com');
+  });
+
+  it('Should save contact successfully', () => {
+    const form = element(by.tagName('form'));
+    const formSubmitButton = element(by.css("input[type='submit']"));
+    formSubmitButton.click();
+    expect(form.isPresent()).toBeFalsy();
+  });
+  
+  
+  it('Contact should have different information', () => {
+    list.get(1).element(by.tagName('a')).click();
+    const detailsPage = element(by.id('contactsDetailsContainer'));
+    expect(detailsPage.getText()).toContain('java@paysafe.com')
+  })
+});
+
+
